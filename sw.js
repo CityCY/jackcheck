d66c24e10199ae7edacaa261cf6f7437c28f31e3
CityCY <96333861+CityCY@users.noreply.github.com>
2026-06-10 09:32:35 +0100

    Add files via upload
    Easier to read bowl numbering

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'jackcheck-v3';
+const CACHE = 'jackcheck-v4';
 const FILES = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', e => {
