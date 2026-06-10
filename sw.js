fc5970be68966cf0d74b472e86f0aee2826c41ad
CityCY <96333861+CityCY@users.noreply.github.com>
2026-06-10 08:56:03 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'jackcheck-v1';
+const CACHE = 'jackcheck-v2';
 const FILES = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', e => {
